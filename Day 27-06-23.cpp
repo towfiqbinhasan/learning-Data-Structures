74d5e34aaa6c706b08c6cd3743810af7e9e4f1c8
towfiqbinhasan <136582948+towfiqbinhasan@users.noreply.github.com>
2023-07-05 00:57:28 +0600

    Update Day 27-06-23.cpp

--- Day 27-06-23.cpp
+++ Day 27-06-23.cpp
@@ -1,31 +1,20 @@
 #include<iostream>
 using namespace std;
-void reversesentence(string s)
-{
-    stack<string> st;
-
-    for(int i=0;i<s.length();i++)
-    {
-        string word="";
-        while(s[i]!=''&&i<s.length())
-        {
-            word+=s[i];
-            i++;
-        }
-        st.push(word);
-
-    }
-    while(!st.empty())
-    {
-
-        cout<<st.top()<<"  ";
-        st.pop();
-    }cout<<endl;
-}
 int main()
 {
+    int a[10]={10,15,45,20,25,6,1,100,65,99};
+    int sum = 0;
+    for (int i=0;i<10;i+=2)
+    {
+        if(sum%2==0)
+            sum=a[i];
+    }
+    cout<<"The result is: "<<sum<<endl;
 
-    string s="Hey, how are you doing?";
-    reversesentence(s);
+
+
+
+
+
     return 0;
 }
