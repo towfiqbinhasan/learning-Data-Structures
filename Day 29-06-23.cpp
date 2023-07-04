89c9e0f6f4f8356b2a33987da37b4ce2f23cfde2
towfiqbinhasan <136582948+towfiqbinhasan@users.noreply.github.com>
2023-07-05 00:41:49 +0600

    Update Day 29-06-23.cpp

--- Day 29-06-23.cpp
+++ Day 29-06-23.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int rows = 3;
+    const int cols = 3;
+
+    int arr[rows][cols];
+
+    cout << "Enter the elements of the array:" <<endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "Enter element at position (" << i << ", " << j << "): ";
+            cin >> arr[i][j];
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            sum += arr[i][j];
+        }
+    }
+
+    cout << "Sum of all elements: " << sum <<endl;
+
+    return 0;
+}
 
