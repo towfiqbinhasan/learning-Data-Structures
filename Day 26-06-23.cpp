8b573c93476f5cf860f7b8cac22ebdc2dce916b9
towfiqbinhasan <136582948+towfiqbinhasan@users.noreply.github.com>
2023-07-05 00:28:14 +0600

    Update Day 26-06-23.cpp

--- Day 26-06-23.cpp
+++ Day 26-06-23.cpp
@@ -1,64 +1,91 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class stack
-{
-    int n;
-    int* a;
+#define MAX_SIZE 100
+
+class Stack {
+private:
     int top;
+    int stack[MAX_SIZE];
+
 public:
-    stack()
-    {
-        a=new int[n];
-        top=-1;
+    Stack() {
+        top = -1;
     }
-    void push(int x)
-    {
-        if(top==n-1)
-            {
-            cout<<"Stack overflow"<<endl;
+
+    bool isEmpty() {
+        return (top == -1);
+    }
+
+    bool isFull() {
+        return (top == MAX_SIZE - 1);
+    }
+
+    void push(int value) {
+        if (isFull()) {
+            cout << "Stack overflow!" <<endl;
             return;
         }
-        top++;
-        a[top]=x;
+        stack[++top] = value;
+        cout << value << " pushed to the stack." <<endl;
     }
-    void pop()
-    {
-    if(top==-1)
-        {
-        cout<<"No element to po"<<endl;
-        return;
+
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack underflow!" <<endl;
+            return;
+        }
+        int value = stack[top--];
+        cout << value << " popped from the stack." <<endl;
     }
-    top--;
-}
-int Top()
-{
-if(top==-1)
-    {
-    cout<<"No element in stak"<<endl;
-    return -1;
-}
-return a[top];
 
-}
-bool empty()
-{
-
-    return top==-1;
-}
+    void display() {
+        if (isEmpty()) {
+            cout << "Stack is empty!" <<endl;
+            return;
+        }
+        cout << "Stack elements: ";
+        for (int i = top; i >= 0; --i) {
+            cout << stack[i] << " ";
+        }
+        cout <<endl;
+    }
 };
-int main()
-{
-    stack st;
-    st.push(1);
-    st.push(2);
-    st.push(3);
-    cout<<st.Top()<<endl;
-    st.pop();
-    st.Top();
-    st.pop();
-    st.pop();
-    st.pop();
-    cout<<st.empty()<<endl;
+
+int main() {
+    Stack stack;
+    int choice, value;
+
+    do {
+        cout << "Stack Menu" <<endl;
+        cout << "1. Push" <<endl;
+        cout << "2. Pop" <<endl;
+        cout << "3. Display" <<endl;
+        cout << "4. Quit" <<endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter a value to push: ";
+                cin >> value;
+                stack.push(value);
+                break;
+            case 2:
+                stack.pop();
+                break;
+            case 3:
+                stack.display();
+                break;
+            case 4:
+                cout << "Exiting..." <<endl;
+                break;
+            default:
+                cout << "Invalid choice. Try again." <<endl;
+        }
+
+        cout <<endl;
+    } while (choice != 4);
+
     return 0;
-};
+}
 
