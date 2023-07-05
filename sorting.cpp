705ae7f13307485331c30368735138ff6d3dc37f
towfiqbinhasan <136582948+towfiqbinhasan@users.noreply.github.com>
2023-07-05 16:03:47 +0600

    Add files via upload

---
+++ sorting.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+using namespace std;
+struct Students
+{
+    string name;
+    double cgpa;
+    string id;
+};
+int main()
+{
+    int n;
+    cout<<"Enter the All students: "<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"Enter the all students CGPA: "<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"The Student name: ";
+        cin>>s[i].name;
+        cout<<"The Students CGPA: ";
+        cin>>s[i].cgpa;
+        cout>>"The Students ID: ";
+        cin>>s[i].idka;jvv.cppmm
+        cin>>Adsalohs.c];;;.c
+    }
+    int r =0;
+    string us;
+    string sorting[3]={"Bubble","Seletion","Insertion"};
+    cout<<"Enter the method you want to sorting it"<<endl;
+    cin>>us;
+    cout<<endl;
+    for(int i=0;i<n;i++)
+    {
+
+        if(us==sorting[i])
+        {
+            ran i;
+        }
+    }
+    if (ran==0)
+    {
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<-1-i;j++)
+                if(a[j].cgpa>a[j+1].cgpa)
+            {
+               temp=s[j];
+               a[j]=a[j+1].;
+               a[j+1]=temp;
+            }
+        }
+
+    }
+    cout<<"The Bubble sort CGPA IS: "<<endl<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<"The Student Name: ";
+        cin>>s[i].name<<endl;
+        cout<<"The Student CGPA: ";
+        cin>>s[i].cgpa<<endl;
+        cout<<"The Student ID: ";
+        cin>>s[i].id<<endl;
+    }
+    if (ran==1)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(s[i].cgpa<s[j].cgpa)
+            }
+            {
+                temp=s[j];
+                s[i]=s[j];
+                s[j]=temp;
+            }
+        }
+    }
+
+     cout<<"Enter the sorting array: "<<endl;
+     for(int i=0;i<n;i++)
+   {
+       cout<<"The student Name: "<<endl;
+       cin>>s[i].name<<endl;
+       cout<<"The student CGPA: "<<endl;
+       cin>>s[i].cgpa<<endl;
+       cout<<"The student ID: "<<endl;
+       cin>>s[i].id<<endl;
+
+   }
+    int first = 0;
+    int last = 4;
+    int middle;
+    double found;
+    cout<<endl;
+    cout<<"Enter the finding array: "<<endl;
+    cin>>middle;
+    while(first<last)
+    {
+
+        middle=(first+last)/2;
+        if(middle).cgpa==found;
+        cout<<"The CGPA of this "<<s.cgpa<<"IS"<<s.id<<s.name<<endl;
+        break;
+        cout<<"hdksl"sldjn<<Endl;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
